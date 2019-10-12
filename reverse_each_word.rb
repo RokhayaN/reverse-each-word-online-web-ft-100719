9c7936fe23b63a4c472cba951c4043d62ca89484
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-12 04:40:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 reverse_each_word = ["Hello everyone , i m rokhaya"]
 def reverse_each_word (words)
 reverse_each_word.reverse ("word")
-reverse_each_word.each do |word|
-reverse_each_word.collect |word|  
+  reverse_each_word.each do |word|
+reverse_each_word.collect do |word|  
 end
-end
-end
+  end
+    end
   
